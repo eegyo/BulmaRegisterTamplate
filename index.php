69897a11bf9ff866bbff5478d63ffb9d18c51716
Your Name <you@example.com>
2023-01-09 23:31:04 +0700

    initial Tamplate Bulma

---
+++ index.php
@@ -0,0 +1,43 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RegApp</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
+
+<body>
+    <section class="section mt-6">
+        <div class="container">
+            <h1 class="title">
+                
+               Register App
+                
+              </h1>
+            <div class="container">
+        <div class="card">
+            <div class="card-content">
+                <div class="content ">
+                    <div class="has-text-weight-bold mt-3">Nama</div>
+                    <input class="input" type="text" placeholder="Name" />
+                    
+
+                    <div class="has-text-weight-bold mt-3">Password</div>
+                    <input class="input" type="password" placeholder="Password" />
+
+
+                    <div class="has-text-weight-bold mt-4">Alamat</div>
+                    <input class="input" type="pasword" placeholder="Alamat" />
+
+                    <button class="button is-link is-full mt-6 is-fullwidth">Register</button>
+                    
+                </div>
+            </div>
+            </div>d
+           </div>
+
+        </div>
+    </section>
+
+ 
+</body>
+</html>
